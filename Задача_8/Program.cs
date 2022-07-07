6e8949284b82eaff76eecb5ba7dc1a37a9a6a349
Tatyana <krupinova@bk.ru>
2022-07-07 18:12:43 +0300

    add Задача 8

---
+++ Задача_8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// Проверить на 5 и 8.
+
+Console.WriteLine("Все чётные числа от 1 до N");
+Console.Write("Введите максимальное число");
+double n = Convert.ToInt32(Console.ReadLine());
+
+for (double result = 1; result <= n; result++)
+{
+    if (result %2 ==0)
+    {
+        Console.Write (result + " ");
+        }
+}
